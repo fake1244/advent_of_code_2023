4772ea311a9a2312f6bcd3b5ee83109236a5af1b
s2615576 <a.petrov-1@student.utwente.nl>
2023-12-13 18:25:50 +0200

    ugly day 13 part 2

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -13,8 +13,7 @@ pub fn solve() -> SolutionPair {
 
     for pattern in input.split("\r\n\r\n") {
         let pattern = pattern.lines().map(|x| x.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
-        // println!("{:?}", pattern);
-        let (r, c) = solve_pattern(&pattern);
+        let (r, c) = solve_pattern(&pattern, false, (0,0));
         sol1 += r * 100 + c;
         let (r, c) = fix_and_solve_pattern(pattern, (r, c));
         sol2 += r * 100 + c;
@@ -24,9 +23,12 @@ pub fn solve() -> SolutionPair {
 }
 
 
-fn solve_pattern(pattern: &Vec<Vec<char>>) -> (usize, usize) {
+fn solve_pattern(pattern: &Vec<Vec<char>>, part2: bool, last_val: (usize, usize)) -> (usize, usize) {
     for line_refl in 1..pattern.len() {
         if zip((0..line_refl).rev(), line_refl..pattern.len()).all(|(l, h)| pattern[l] == pattern[h]){
+            if part2 && (line_refl, 0) == last_val{
+                continue;
+            }
             return (line_refl, 0);
         }
     }
@@ -39,6 +41,9 @@ fn solve_pattern(pattern: &Vec<Vec<char>>) -> (usize, usize) {
 
     for line_refl in 1..pattern.len() {
         if zip((0..line_refl).rev(), line_refl..pattern.len()).all(|(l, h)| pattern[l] == pattern[h]){
+            if part2 && (0, line_refl) == last_val{
+                continue;
+            }
             return (0, line_refl);
         }
     }
@@ -46,41 +51,23 @@ fn solve_pattern(pattern: &Vec<Vec<char>>) -> (usize, usize) {
     (0, 0)
 }
 
+
 fn fix_and_solve_pattern(pattern: Vec<Vec<char>>, last_res: (usize, usize)) -> (usize, usize) {
     let mut new_pattern = pattern.clone();
-    let mut start_row = 0;
-    let mut start_col = 0;
-    // if 2*last_res.0 > pattern.len() {
-    //     start_row = 2*last_res.0 - pattern.len();
-    // }
-    // if 2*last_res.1 > pattern[0].len() {
-    //     start_col = 2*last_res.1 - pattern[0].len();
-    // }
-    println!("{} {}, {} {}", last_res.0, last_res.1, start_row, start_col);
     for r in start_row..pattern.len() {
         for c in start_col..pattern[r].len() {
             if pattern[r][c] == '#' {
                 new_pattern[r][c] = '.';
-                let new_res = solve_pattern(&new_pattern);
+                let new_res = solve_pattern(&new_pattern, true, last_res);
                 if new_res != (0, 0) && new_res != last_res {
-                    // for line in &new_pattern {
-                    //     println!("{:?}", line);
-                    // }
-                    // println!("{} {}; {} {}", r, c, new_res, last_res);
                     return new_res;
-                    // max_res = new_res;
                 }
                 new_pattern[r][c] = '#';
             } 
             else if pattern[r][c] == '.' {
                 new_pattern[r][c] = '#';
-                let new_res = solve_pattern(&new_pattern);
+                let new_res = solve_pattern(&new_pattern, true, last_res);
                 if new_res != (0, 0) && new_res != last_res {
-                    // for line in &new_pattern {
-                    //     println!("{:?}", line);
-                    // }
-                    // println!("{} {}; {} {}", r, c, new_res, last_res);
-                    // max_res = new_res;
                     return new_res;
                 }
                 new_pattern[r][c] = '.';
@@ -89,7 +76,3 @@ fn fix_and_solve_pattern(pattern: Vec<Vec<char>>, last_res: (usize, usize)) -> (
     }
     (0, 0)
 }
-
-// wrong 22673 too low
-// 936
-// correct 28235
